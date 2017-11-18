357dc6f46b3f79687b002dda1701094f40fcbad4
vanilaboy <slawa128128@gmail.com>
2017-11-18 17:32:11 +0300

    card_layout

--- app/src/main/java/com/ilonmask/root/reciepesapp/StartedPage.java
+++ app/src/main/java/com/ilonmask/root/reciepesapp/StartedPage.java
@@ -1,25 +1,36 @@
 package com.ilonmask.root.reciepesapp;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class StartedPage extends AppCompatActivity {
+
+    private LinearLayout linear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getCallingActivity()
-
-
         setContentView(R.layout.activity_started_page);
+
+        linear = (LinearLayout)findViewById(R.id.linearLayout);
+        fillingCards();
+
     }
 
-    private void fillingCards(Bundle savedInstanceState){
+    private void fillingCards(){
         //запрос к серверу
 
-        Context context = getActivity().getApplicationContext();
-        LinearLayout layout =
+
+        for(int i = 0; i < 20; i++) {
+            String result = "Result";
+            View view = getLayoutInflater().inflate(R.layout.card_layout, null);
+            TextView textView = (TextView) view.findViewById(R.id.info_text);
+            result = result + i;
+            textView.setText(result);
+            linear.addView(view);
+        }
     }
 }
